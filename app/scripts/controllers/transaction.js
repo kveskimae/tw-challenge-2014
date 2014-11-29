ab8b1bb5d627c6ff07bbcc59f4b6c0cfc0f2244a
Dmitri Timasjov <timasjov.dmitri@gmail.com>
2014-11-29 17:42:56 +0200

    allows to to transactions

---
+++ app/scripts/controllers/transaction.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name djoisajdoijdsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the djoisajdoijdsApp
+ */
+angular.module('djoisajdoijdsApp')
+  .controller('TransactionCtrl', function ($scope, $location) {
+      $scope.amount = 0;
+      $scope.fee = 0;
+      if($scope.amount != 0) {
+        console.log(11);
+        $scope.fee = $scope.amount / 1000;
+      }
+
+      $scope.back = function() {
+        $location.path("/friends");
+      };
+      $scope.confirm = function() {
+        //TODO: show confirmation message
+        $location.path("/friends");
+      };
+
+  });
