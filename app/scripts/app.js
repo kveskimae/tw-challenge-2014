04ca2f3292300907ff9325042ee4ab7365ea59cf
Dmitri Timasjov <timasjov.dmitri@gmail.com>
2014-11-29 16:02:47 +0200

    implemented transactions history functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/history', {
+        templateUrl: 'views/history.html',
+        controller: 'HistoryCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
