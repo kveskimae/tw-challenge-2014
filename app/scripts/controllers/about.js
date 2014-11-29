9b101884fa49b16170a21148f0eba278457b1adc
Ivar Mahhonin <ivar.mahhonin@gmail.com>
2014-11-29 15:00:06 +0200

    initial template commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name djoisajdoijdsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the djoisajdoijdsApp
+ */
+angular.module('djoisajdoijdsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
